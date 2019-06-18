fc85cbad5641573f560f3e67e4f3c41c9e8febd1
Joshua <olajidejoshua4real@gmail.com>
2019-06-18 16:52:10 +0100

    Get profile function added to get users profile name

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ function printMessage(username, badgeCount, points) {
     const message = `${username} has ${badgeCount} total badge(s) and ${points} points in Javascript`;
     console.log(message);
 }
-// Connect to the API URL (https://teamtreehouse.com/username.json)
+function getProfile(username) {
+    // Connect to the API URL (https://teamtreehouse.com/username.json)
     const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
             let body = "";
             // Read the Data
@@ -20,9 +21,13 @@ function printMessage(username, badgeCount, points) {
             response.on('end', () => {
                  // Parse the data
                 const profile = JSON.parse(body);
-                console.dir(profile);
+                // console.dir(profile);
+                printMessage(username, profile.badges.length, profile.points.JavaScript);
                 // Print the data
             })
            
             
     });
+}
+getProfile("chalkers");
+getProfile("alenaholligan");
