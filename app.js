570a3caaa166c54d6de2a13fb5b77f6919ad2bfd
Joshua <olajidejoshua4real@gmail.com>
2019-06-18 16:00:31 +0100

    Parameters added to the function

--- app.js
+++ app.js
@@ -3,7 +3,11 @@
 //Solution: Use Node.js to connect to treehouse's API to get profile information to print out
 
 //Function to print message to the console
-function printMessage()
+function printMessage(username, badgeCount, points) {
+    const message = `${username} has ${badgeCount} total badge(s) and ${points} points in Javascript`;
+    console.log(message);
+}
+printMessage("Joshua", 100, 2000000);
 // Connect to the API URL (https://teamtreehouse.com/username.json)
 // Read the Data
 // Parse the data
