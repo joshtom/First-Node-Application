6cc7911f8081090cf844ca685e26b41da49b700a
Joshua <olajidejoshua4real@gmail.com>
2019-06-19 14:16:38 +0100

    Create module print.js then exported it into the profile.js file

--- profile.js
+++ profile.js
@@ -4,15 +4,13 @@ const https = require('https');
 // Require http module for status code
 const http = require('http');
 
+const printMsg = require('./print.js');
+
 //Print error Messages
 function printError(error) {
     console.log(error.message);
 }
-//Function to print message to the console
-function printMessage(username, badgeCount, points) {
-    const message = `${username} has ${badgeCount} total badge(s) and ${points} points in Javascript`;
-    console.log(message);
-}
+
 function get(username) {
     try{
     // Connect to the API URL (https://teamtreehouse.com/username.json)
@@ -28,7 +26,7 @@ function get(username) {
                     // Parse the data
                     const profile = JSON.parse(body);
                     // console.dir(profile);
-                    printMessage(username, profile.badges.length, profile.points.JavaScript);
+                    printMsg.printMessage(username, profile.badges.length, profile.points.JavaScript);
                     // Print the data
                 } catch(error) {
                     printError(error);
